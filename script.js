99776085a8f333cfc8b7604ad1cfc500a2e8ce33
Ahmad Haymour <haimour22@gmail.com>
2022-03-31 19:44:05 +0200

    A

--- script.js
+++ script.js
@@ -3,11 +3,35 @@ const myBtn = document.getElementById('button-addon1')
 
 const text = document.querySelector('.myPara')
 
+let subs = 0
+
 myBtn.addEventListener("click", (event) => {
 
+    // if ( d.getDay() != -1){
+    //     subs = 6 - (d.getDay() + 1 )
+    // }
+
+    subs = 6 - (date.getDay() ) 
+
     let name = document.querySelector('input').value
-    text.textContent = `Halo ${name}. Today is Thursday only 1 day left until Weekend!`
-
+    text.textContent = `Halo ${name}. Today is ${day} only ${subs} day left until Weekend!`
     console.log(name);
-
 })
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  const date = new Date();
+
+  console.log(date.getDay());
+  let day = days[date.getDay()];  
+  document.getElementById("demo").innerHTML = `Today is: ${day}`;
+
+  console.log(day);
